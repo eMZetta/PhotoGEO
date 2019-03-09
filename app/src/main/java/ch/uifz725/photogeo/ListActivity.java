92e4ba651b4b52a8c429d594803ec20dd2de1860
eMZetta <michi.zihlmann@gmx.net>
2019-03-09 15:16:46 +0100

    Details/Stuff made in class

---
+++ app/src/main/java/ch/uifz725/photogeo/ListActivity.java
@@ -0,0 +1,17 @@
+package ch.uifz725.photogeo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ListActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
